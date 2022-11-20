1ee1519f8897ca9d5599ff8f4f6281246e0573dd
kz-sik <115482594+kz-sik@users.noreply.github.com>
2022-11-20 12:54:17 +0000

    add address model table

--- config/routes.rb
+++ config/routes.rb
@@ -10,9 +10,9 @@ Rails.application.routes.draw do
   }
 
   root to: 'homes#top'
-  get 'home/about' => 'homes#about', as: 'about'
+  get '/about' => 'homes#about', as: 'about'
 
-  resources :items, only[:index, :show]
+  resources :items, only: [:index, :show]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
