796b9ad211c1c8c2269f59b819103ad82a25c4a8
kz-sik <115482594+kz-sik@users.noreply.github.com>
2022-11-24 14:49:44 +0000

    add genre index edit update

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -6,7 +6,7 @@ class Admin::GenresController < ApplicationController
   end
 
   def create
-    genre = Genre.new
+    genre = Genre.new(genre_params)
     genre.save
     redirect_to admin_genres_path
   end
@@ -25,6 +25,6 @@ class Admin::GenresController < ApplicationController
   private
 
   def genre_params
-    params.require(:@genre).permit(:name)
+    params.require(:genre).permit(:name)
   end
 end
