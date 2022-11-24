686a3690b72e532b999429d944f2f77ddb0706e4
kz-sik <115482594+kz-sik@users.noreply.github.com>
2022-11-24 15:21:56 +0000

    add items new

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -7,6 +7,9 @@ class Admin::ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to admin_item_path(@item.id)
   end
 
   def show
@@ -16,5 +19,11 @@ class Admin::ItemsController < ApplicationController
   end
 
   def update
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :genre_id, :name, :introduction, :price, :is_active)
   end
 end
