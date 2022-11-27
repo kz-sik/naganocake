a1452a82e14be6e56dd1086c2092830044c0ce79
kz-sik <115482594+kz-sik@users.noreply.github.com>
2022-11-27 15:20:10 +0000

    add admin customer index kaminari

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,5 +1,6 @@
 class Admin::CustomersController < ApplicationController
   def index
+    @customers = Customer.page(params[:page])
   end
 
   def show
@@ -9,5 +10,11 @@ class Admin::CustomersController < ApplicationController
   end
 
   def update
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :encrypted_password, :pastal_code, :address, :telephone_number, :is_deleted)
   end
 end
