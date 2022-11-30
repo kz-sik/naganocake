a50f7e866d321b7807d49874601f860f28d8669d
kz-sik <115482594+kz-sik@users.noreply.github.com>
2022-11-30 15:56:29 +0000

    add order show

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -2,4 +2,9 @@ class Admin::HomesController < ApplicationController
   def top
     @order_details = Order_detail.all
   end
+
+  private
+  def home_params
+    params.require(:home).permit(:order_details_id)
+  end
 end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,7 +1,14 @@
 class Admin::OrdersController < ApplicationController
   def show
+    @order = order.find(params.id)
   end
 
   def update
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:customer_id, :postal_code, :address, :name, :postage, :price, :payment, :order_status)
   end
 end
