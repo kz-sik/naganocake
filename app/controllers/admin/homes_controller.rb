44493b065703aefc529a2464c55356eccb9e7287
kz-sik <115482594+kz-sik@users.noreply.github.com>
2022-12-01 13:45:05 +0000

    add public top

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,10 +1,6 @@
 class Admin::HomesController < ApplicationController
   def top
-    @order_details = Order_detail.all
+    @order = Order.all
   end
 
-  private
-  def home_params
-    params.require(:home).permit(:order_details_id)
-  end
 end
