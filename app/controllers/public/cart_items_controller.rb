dbd0822c131251e8796a300eb3497029615ccc84
kz-sik <115482594+kz-sik@users.noreply.github.com>
2022-12-07 00:44:02 +0000

    add public cartitem controller

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,13 +1,26 @@
 class Public::CartItemsController < ApplicationController
   def index
     @cart_items = CartItem.all
-    @cart_items.customer.id = current_customer.
   end
 
   def update
+    @cart_item = CartItem.find(item_id: params[:cart_item][:item_id])
+    @cart_item.update(amount: @cart_item.amount)
+    redirect_to cart_item_path
   end
 
   def create
+    @cart_item = CartItem.find_by(item_id: params[:cart_item][:item_id])
+    if  @cart_item.present?
+      @cart_item.amount += cart_item_params[:amount].to_i
+      @cart_item.update(amount: @cart_item.amount)
+      redirect_to cart_items_path
+    else
+      @cart_item = CartItem.new(cart_item_params)
+      @cart_item.customer_id = current_customer.id
+      @cart_item.save
+      redirect_to cart_items_path
+    end
   end
 
   def destroy
