4a39433a8296f9cf6abae3fcbe03d0d1ec51a906
kz-sik <115482594+kz-sik@users.noreply.github.com>
2022-12-17 14:44:07 +0000

    add enum

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -10,6 +10,7 @@ class Public::OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
+    @order_details = @order.order_details
   end
 
   def comfirm
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,9 @@
 class Order < ApplicationRecord
   has_many :order_details
   belongs_to :customer
-  
-  
-  
-  enum payment_method: { credit_card: 0, transfer: 1 }
+
+
+
+  enum payment: { credit_card: 0, transfer: 1 }
+  enum order_status: { before_payment: 0, payment_confirmation: 1, in_production: 2, delivery_preparation: 3, delivered: 4 }
 end
