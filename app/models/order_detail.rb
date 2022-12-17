60d77780cf705a2f60a7aea945aa302c7f246aa5
kz-sik <115482594+kz-sik@users.noreply.github.com>
2022-12-17 15:20:16 +0000

    add order index show

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -2,5 +2,10 @@ class OrderDetail < ApplicationRecord
   belongs_to :item
   belongs_to :order
 
+
+  def subtotal
+    price * amount
+  end
+
   enum crafting_status: { production_not_possible: 0, production_pending: 1, in_production: 2, production_complete: 3 }
 end
