7a532e710c6e6c25963a7e94ccba51220fe6cb10
kz-sik <115482594+kz-sik@users.noreply.github.com>
2022-12-18 15:53:50 +0000

    add admin top order show

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,9 +1,17 @@
 class Admin::OrdersController < ApplicationController
   def show
-    @order = order.find(params.id)
+    @order = Order.find(params[:id])
+    @order_details = @order.order_details.all
   end
 
   def update
+    @order = Order.find(params[:id])
+    @order.order_status = params[:order][:order_status].to_i
+    @order.update(order_status: @order.order_status)
+    @order_details = @order.order_details.all
+    @order_details.crafting_status = "1".to_i
+    @order_details.update(crafting_status: @order_details.crafting_status)
+    redirect_to admin_order_path(params[:id])
   end
 
   private
--- app/controllers/admin/orders_detail_controller.rb
+++ app/controllers/admin/orders_detail_controller.rb
@@ -1,4 +1,9 @@
 class Admin::OrdersDetailController < ApplicationController
   def update
+    @order = Order.find(params[:id])
+    @order_details = @order.order_details.all
+    @order_details.crafting_status = params[:order_details][:crafting_status].to_i
+    @order_details.update(crafting_status: @order_details.crafting_status)
+    redirect_to admin_order_path(params[:id])
   end
 end
